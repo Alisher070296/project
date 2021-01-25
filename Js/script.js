d771b3f57f975b0d727e9bac9b0ce71f01b7f618
Alisher <alisher.b.k.b.k@gmail.com>
2021-01-25 13:43:48 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// let a = prompt('тебе 18 ?','');
+// console.log(typeof(a));
+// ____________
+// const a = alert('Hello');
+// console.log(a);
+// _______________
+// const a = confirm('тебе 18 ?');
+// console.log(a);
+//_________________
+
+// const results = [];
+// results[0]= prompt('Как ваше имя ?');
+// results[1]= prompt('Как ваше фамилия ?');
+// results[2]= prompt('Сколько тебе лет ?');
+// document.write(results);
+
+// const user = 'Ali';
+// alert(`Привет ${user}`);
+
+// const page = 'photo';
+// console.log(`https://google.com/${page}`);
+
